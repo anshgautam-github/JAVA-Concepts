f174bc2725e1b1a5c0bd8838982b19a98783f800
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-16 18:33:51 +0530

    Create BoxColor.java

---
+++ OOPS/01- Inheritance/BoxColor.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class BoxColor extends BoxWeight{
+
+	
+	//heirarchial inheritance bcoz both price and colour inherit the BOxWeight
+
+}
+
+
+
+
+//public class BoxColor extends BoxColor{
+//
+//	
+//	//Cyclic inheritance NOT POSIBBLE
+//
+//}
