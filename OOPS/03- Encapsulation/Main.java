9491ca99a23d42b50f36adf73594cc54ea6be007
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-17 18:16:53 +0530

    Update Main.java

--- OOPS/03- Encapsulation/Main.java
+++ OOPS/03- Encapsulation/Main.java
@@ -1,1 +1,34 @@
 
+
+Abstraction means hiding the unneccessary details but showing the essential information, start the car,
+get the elemt. SO this is the design issue. How is it actually being desgined, the desginers will tell you
+thay, hey, I need some sort of abstraction and give me this method, this method,etc. And whatver u put inside 
+this , is upto you, whatver, how u do it, how u encapsulate it, how u put methods in it, how u put class, 
+how u make it work internally that is upto you. 
+All that dedigners will say -> they need is just these 4 methods.
+So these are the deginers people those will tell you these things.
+
+The people who were woking on the implementation part -> the engineers for eg-> they will be like ok , 
+we are working on the implementation , u want this sort of abstraction , we will achieve it via this class , 
+via making this particular method , via making this get method, set method.
+
+So the actual writing of code of that ENCAPSULATED thing-> is providing us ABSTRACTION.
+That is the main difference.
+
+Abstraction is solving design level issue 	
+Encapsulation is solving implementation level issue 
+
+Encapsulation is -> it hides the code in the single entity or whatver and protects it from the outside world.
+Abstarction on the other hand->  is telling us okay, encapsualtion did all these things , hiding smtg internally
+, i am abstractiona nd i am providing u some of the outside methods u can use (design way).
+But how this thing is working internally -> is handlled by ENCPSULATION. 
+
+One more simple explaiantion -> 
+Abstraction focuses on the external stuff and ENCAP-> focuses on the internal working 
+
+Abstraction is the process of gaining info, ENCAP-> process for containing info
+
+DATA HIDING vs ENCAPSULATION ->
+DH-> used for security and provate things
+ENCAP-> used to hide the complexity of the system
+ENCAP is a sub process in DATA hiding
